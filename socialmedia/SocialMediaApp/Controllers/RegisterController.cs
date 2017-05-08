98076d8f1ce0d59baf05b62c029c7a1cdefbd5f7
GitHubQuindao <quindao.markjeasu@gmail.com>
2017-05-08 11:51:23 +0800

    Backend register

---
+++ socialmedia/SocialMediaApp/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialMediaApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Register() {
+            return View();
+        }
+
+     
+    }
+}
---
+++ socialmedia/backend/Services/Models/Entities/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Services.Models.Entities
+{
+    public class Register
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required, StringLength(50)]
+        public string UserEmail { get; set; }
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+        public string UserGender { get; set; }
+        public string UserBday { get; set; }
+    }
+}
